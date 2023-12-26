3b0bd3cc55a452ee17cb0d9a6c42c424726971bc
IrynaKonovalowa <iri.moummi@gmail.com>
2023-12-26 21:05:38 +0100

    #Task3  Добавлен метод Receive.
    Позволяет осуществить регистрацию, авторизацию и редактирование пользователя.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using MessengerModel;
 using System.Runtime.Serialization.Json;
 using System.Net.Sockets;
 using System.Net;
+using CommandDLL;
 
 
 
@@ -10,6 +11,8 @@ namespace ServerAPP
 {
     internal class Server
     {
+        NetworkStream netstream;
+        TcpClient client;
         static void Main(string[] args)
         {
             Server s = new Server();
@@ -32,13 +35,65 @@ namespace ServerAPP
                         // Принимаем ожидающий запрос на подключение 
                         // Метод AcceptTcpClient — это блокирующий метод, возвращающий объект TcpClient, 
                         // который может использоваться для приема и передачи данных.
-                        TcpClient client = await listener.AcceptTcpClientAsync();
-                        //Receive(client);
+                        client = await listener.AcceptTcpClientAsync();
+                        Receive(client);
                     }
                 }
                 catch (Exception ex)
                 {
                     WriteLine("Сервер: " + ex.Message);
+                }
+            });
+        }
+        private async void Receive(TcpClient tcpClient)
+        {
+            await Task.Run(async () =>
+            {
+                try
+                {
+                    // Получим объект NetworkStream, используемый для приема и передачи данных.
+                    netstream = tcpClient.GetStream();
+                    byte[] arr = new byte[tcpClient.ReceiveBufferSize /* размер приемного буфера */];
+                    // Читаем данные из объекта NetworkStream.
+                    int len = await netstream.ReadAsync(arr, 0, tcpClient.ReceiveBufferSize);// Возвращает фактически считанное число байтов
+                    while (true)
+                    {
+                        if (len == 0)
+                        {
+                            netstream.Close();
+                            tcpClient.Close(); // закрываем TCP-подключение и освобождаем все ресурсы, связанные с объектом TcpClient.
+                            return;
+                        }
+                        // Создадим поток, резервным хранилищем которого является память.
+                        byte[] copy = new byte[len];
+                        Array.Copy(arr, 0, copy, 0, len);
+                        MemoryStream stream = new MemoryStream(copy);
+                        var jsonFormatter = new DataContractJsonSerializer(typeof(Wrapper));
+                        Wrapper wr = jsonFormatter.ReadObject(stream) as Wrapper;// выполняем десериализацию
+                        wr.user.IPadress = tcpClient.Client.RemoteEndPoint.ToString();// информация об удаленном хосте, который отправил датаграмму
+                        switch (wr.commands)
+                        {
+                            case Wrapper.Commands.Registratioin:
+                                //RegistratioinUser(wr.user);
+                                break;
+                            case Wrapper.Commands.Authorization:
+                                //AuthorizationUser(wr.user);
+                                break;
+                            case Wrapper.Commands.Redact:
+                                //RedactUser(wr.user);
+                                break;
+                        }
+                        stream.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    WriteLine("Сервер: " + ex.Message);
+                }
+                finally
+                {
+                    netstream?.Close();
+                    tcpClient?.Close(); // закрываем TCP-подключение и освобождаем все ресурсы, связанные с объектом TcpClient.
                 }
             });
         }
