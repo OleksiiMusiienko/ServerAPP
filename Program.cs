27742ccbdf047142a2354751c460f367bebc5ec0
Виктория <babenko.viktoria.v@gmail.com>
2024-01-15 19:04:03 +0200

    #Task16 Реализация методов обменна сообщениями

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Channels;
 using System.Collections.Generic;
 using System.Net.Http;
+using Azure;
 
 
 
@@ -17,10 +18,13 @@ namespace ServerAPP
     {
         ServerResponse response = new ServerResponse();
         List<NetworkStream> clients = new List<NetworkStream>();
+        List <TcpClient> tcpClients = new List<TcpClient>();
+        List<Message> listMes = new List<Message>();
         static void Main(string[] args)
         {
             Server s = new Server();
             s.Accept();
+            s.AcceptMessender();
             Read();
         }
         private async void Accept()
@@ -188,7 +192,7 @@ namespace ServerAPP
                        u.Write(arr, 0, arr.Length); 
                     }
                 }
-                }
+            }
             catch (Exception ex)
             {
                 WriteLine("Сервер: " + ex.Message);
@@ -331,5 +335,162 @@ namespace ServerAPP
                 WriteLine("Сервер: " + ex.Message);
             }
         }
+        // Реализация методов обменна сообщениями 
+        private async void AcceptMessender()
+        {
+            await Task.Run(async () =>
+            {
+                try
+                {
+                    // TcpListener ожидает подключения от TCP-клиентов сети.
+                    TcpListener lis = new TcpListener(
+                    IPAddress.Any /* Предоставляет IP-адрес, указывающий, что сервер должен контролировать действия клиентов на всех сетевых интерфейсах.*/,
+                    49153 /* порт */);
+                    lis.Start(); // Запускаем ожидание входящих запросов на подключение
+                    while (true)
+                    {
+                        // Принимаем ожидающий запрос на подключение 
+                        // Метод AcceptTcpClient — это блокирующий метод, возвращающий объект TcpClient, 
+                        // который может использоваться для приема и передачи данных.
+                        TcpClient client = await lis.AcceptTcpClientAsync();
+                        tcpClients.Add(client);
+                        ReceiveMessender(client);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    WriteLine("Сервер: " + ex.Message);
+                }
+            });
+        }
+        private async void ReceiveMessender(TcpClient tcpClient)
+        {
+            await Task.Run(async () =>
+            {
+                NetworkStream netstream = null;
+
+                try
+                {
+                    // Получим объект NetworkStream, используемый для приема и передачи данных.
+                    netstream = tcpClient.GetStream();
+                    byte[] arr = new byte[tcpClient.ReceiveBufferSize /* размер приемного буфера */];
+                    // Читаем данные из объекта NetworkStream.
+                    while (true)
+                    {
+                        int len = await netstream.ReadAsync(arr, 0, tcpClient.ReceiveBufferSize);// Возвращает фактически считанное число байтов
+
+                        if (len == 0)
+                        {
+                            netstream.Close();
+                            tcpClient.Close(); // закрываем TCP-подключение и освобождаем все ресурсы, связанные с объектом TcpClient.
+                            return;
+                        }
+                        // Создадим поток, резервным хранилищем которого является память.
+                        byte[] copy = new byte[len];
+                        Array.Copy(arr, 0, copy, 0, len);
+                        MemoryStream stream = new MemoryStream(copy);
+                        var jsonFormatter = new DataContractJsonSerializer(typeof(Message));
+                        Message mes = jsonFormatter.ReadObject(stream) as Message;// выполняем десериализацию
+
+                        if (mes.Mes != null) 
+                        {
+                            NewMessage(netstream, mes);
+                        }
+                        else 
+                        {
+                            HistoryMessage(netstream, mes);
+                        }
+                        stream.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    WriteLine("Сервер: " + ex.Message);
+                }
+                finally
+                {
+                    netstream?.Close();
+                    tcpClient?.Close(); // закрываем TCP-подключение и освобождаем все ресурсы, связанные с объектом TcpClient.
+                }
+            });
+        }
+        private void HistoryMessage(NetworkStream netstream, Message mes, TcpClient client = null)
+        {
+            try
+            {
+                //проверяем есть ли такой пользователь в BD
+                using (var db = new MessengerContext())
+                {
+                    var query = from b in db.Messages
+                                where b.UserSenderId == mes.UserSenderId && b.UserRecepientId == mes.UserRecepientId
+                                select b;
+                    listMes = new List<Message>();
+
+                    foreach (var b in query)
+                    {
+                        Message message = new Message();
+                        message.UserSenderId = b.UserSenderId;
+                        message.UserRecepientId = b.UserRecepientId;
+                        message.Date_Time = b.Date_Time;
+                        message.Mes = b.Mes;
+                        listMes.Add(message);
+                    }
+                    MemoryStream stream = new MemoryStream();
+                    var jsonFormatter = new DataContractJsonSerializer(typeof(List<Message>));
+                    jsonFormatter.WriteObject(stream, listMes);
+                    byte[] arr = stream.ToArray(); // записываем содержимое потока в байтовый массив
+                    stream.Close();
+                    netstream.Write(arr, 0, arr.Length);
+                    if (client != null) 
+                    {
+                        NetworkStream netstreamUserRecepient = null;
+                        netstreamUserRecepient = client.GetStream();
+                        netstreamUserRecepient.Write(arr, 0, arr.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Сервер: " + ex.Message);
+            }
+        }
+        private void NewMessage(NetworkStream netstream, Message mes)
+        {
+            try
+            {
+                //проверяем есть ли такой пользователь в BD
+                using (var db = new MessengerContext())
+                {
+                    User user = new User();
+                    var query = from b in db.Users
+                                where b.Id == mes.UserSenderId && b.Id == mes.UserRecepientId
+                                select b;
+                    foreach (var b in query)
+                    {
+                        if (b.Id == mes.UserRecepientId)
+                            user = b;
+                    }
+                    if (query != null)
+                    {
+                        db.Messages.Add(mes);
+                    }
+                    TcpClient UserRecepient = null;
+                    foreach (var tsp in tcpClients)
+                    {
+                        string ip = tsp.Client.RemoteEndPoint.ToString().Substring(0, 13);
+                        if (user.IPadress == ip)
+                        {
+                            UserRecepient = tsp;
+                        }
+                    }
+                    HistoryMessage(netstream, mes, UserRecepient);
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Сервер: " + ex.Message);
+            }
+        }
     }
 }
+    
